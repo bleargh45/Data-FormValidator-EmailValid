586cadc2956caccee16a6eff77a8948518ede768
Graham TerMarsch <graham@howlingfrog.com>
2008-01-08 22:28:54 +0000

    Renamed methods to {{{FV_email_filter()}}} and {{{FV_email()}}}, and changed the behaviour of the filter so that it auto-mangles e-mail addresses to lower-case.

--- lib/Data/FormValidator/EmailValid.pm
+++ lib/Data/FormValidator/EmailValid.pm
@@ -12,17 +12,17 @@ use Email::Valid;
 ###############################################################################
 use base qw( Exporter );
 our @EXPORT_OK = qw(
-    filter_email_valid
-    constrain_email_valid
+    FV_email_filter
+    FV_email
     );
 
 ###############################################################################
 # Version number.
 ###############################################################################
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 ###############################################################################
-# Subroutine:   filter_email_valid(%options)
+# Subroutine:   FV_email_filter(%options)
 # Parameters:   %options    - Options for Email::Valid
 ###############################################################################
 # Filter method which cleans up the given value and returns valid e-mail
@@ -31,15 +31,22 @@ our $VERSION = '0.01';
 # "Valid" is deemed to mean "looks like an e-mail"; no other tests are done to
 # ensure that a valid MX exists or that the address is actually deliverable.
 #
+# This filter method automatically converts all e-mail addresses to lower-case.
+# This behaviour can be disabled by passing through an 'lc=>0' option.
+#
 # You may also pass through any additional 'Email::Valid' '%options' that you
 # want to use; they're handed straight through to 'Email::Valid'.
 ###############################################################################
-sub filter_email_valid {
+sub FV_email_filter {
     my %options = @_;
+    # check if we should mangle the e-mail to all lower-case (default yes)
+    my $mangle_lc = 1;
+    $mangle_lc = delete $options{'lc'} if (exists $options{'lc'});
+    # return filter closure
     return sub {
         my $email = shift;
         return Email::Valid->address(
-                    '-address'  => $email,
+                    '-address'  => $mangle_lc ? lc($email) : $email,
                     '-fudge'    => 1,
                     '-mxcheck'  => 0,
                     %options,
@@ -48,7 +55,7 @@ sub filter_email_valid {
 }
 
 ###############################################################################
-# Subroutine:   constrain_email_valid(%options)
+# Subroutine:   FV_email(%options)
 # Parameters:   %options    - Options for Email::Valid
 ###############################################################################
 # Constraint method which checks to see if the value being constrained is a
@@ -64,7 +71,7 @@ sub filter_email_valid {
 # You can also pass through any additional 'Email::Valid' '%options' that you
 # want to use; they're handed straight through to 'Email::Valid'.
 ###############################################################################
-sub constrain_email_valid {
+sub FV_email {
     my %options = @_;
     return sub {
         my $dfv = shift;
@@ -88,17 +95,17 @@ Data::FormValidator::EmailValid - Data::FormValidator e-mail address constraint/
 
 =head1 SYNOPSIS
 
-  use Data::FormValidator::EmailValid qw(filter_email_valid constrain_email_valid);
+  use Data::FormValidator::EmailValid qw(FV_email_filter FV_email);
 
   $results = Data::FormValidator->check(
         { 'email' => 'Graham TerMarsch <cpan@howlingfrog.com>',
         },
         { 'required' => [qw( email )],
           'field_filters' => {
-              'email' => filter_email_valid(),
+              'email' => FV_email_filter(),
           },
           'constraint_methods' => {
-              'email' => constrain_email_valid(),
+              'email' => FV_email(),
           },
         );
 
@@ -110,14 +117,14 @@ using C<Email::Valid>.
 
 Although I generally find that I'm using the filter and constraint together,
 they've been separated so that you could use just one or the other (e.g. you
-may want to constrain on valid e-mail addresses without actually modifying any
-of the data provided to you by the user).
+may want to constrain on valid e-mail addresses without actually cleaning up or
+filtering any of the data provided to you by the user).
 
 =head1 METHODS
 
 =over
 
-=item filter_email_valid(%options)
+=item FV_email_filter(%options)
 
 Filter method which cleans up the given value and returns valid e-mail
 addresses (or nothing, if the value isn't a valid e-mail address). 
@@ -126,10 +133,14 @@ addresses (or nothing, if the value isn't a valid e-mail address).
 to ensure that a valid MX exists or that the address is actually
 deliverable. 
 
+This filter method automatically converts all e-mail addresses to
+lower-case. This behaviour can be disabled by passing through an
+C<lc=E<gt>0> option. 
+
 You may also pass through any additional C<Email::Valid> C<%options> that
 you want to use; they're handed straight through to C<Email::Valid>. 
 
-=item constrain_email_valid(%options)
+=item FV_email(%options)
 
 Constraint method which checks to see if the value being constrained is a
 valid e-mail address or not. Returns true if the e-mail address is valid,
--- t/email.t
+++ t/email.t
@@ -1,36 +1,60 @@
 use strict;
 use warnings;
-use Test::More tests => 4;
+use Test::More tests => 10;
 use Data::FormValidator;
 BEGIN {
-    use_ok( 'Data::FormValidator::EmailValid', qw(filter_email_valid constrain_email_valid) );
+    use_ok( 'Data::FormValidator::EmailValid', qw(FV_email_filter FV_email) );
 }
 
 ###############################################################################
-test_filter_email_valid: {
+test_filter: {
     my $results = Data::FormValidator->check(
-        { 'email' => 'Graham TerMarsch <cpan@howlingfrog.com>',
+        { 'good'                => 'test@example.com',
+          'good-with-name'      => 'Test User <test@example.com>',
+          'good-case-mangled'   => 'TEST@eXaMpLe.CoM',
+          'good-case-preserved' => 'TEST@eXaMpLe.CoM',
+          'bad-no-domain'       => 'test@',
+          'bad-no-user'         => '@example.com',
+          'bad-not-email'       => 'not an e-mail address',
         },
-        { 'required' => [qw( email )],
+        { 'required' => [qw(
+            good good-with-name good-case-mangled good-case-preserved
+            bad-no-domain bad-no-user bad-not-email
+            )],
           'field_filters' => {
-              'email' => filter_email_valid(),
+              'good'                => FV_email_filter(),
+              'good-with-name'      => FV_email_filter(),
+              'good-case-mangled'   => FV_email_filter(),
+              'good-case-preserved' => FV_email_filter(lc=>0),
+              'bad-no-domain'       => FV_email_filter(),
+              'bad-no-user'         => FV_email_filter(),
+              'bad-not-email'       => FV_email_filter(),
           },
         } );
-    is( $results->valid('email'), 'cpan@howlingfrog.com', 'e-mail filtered ok' );
+    is(  $results->valid('good'),                   'test@example.com', 'filter: good' );
+    is(  $results->valid('good-with-name'),         'test@example.com', 'filter: good-with-name' );
+    is(  $results->valid('good-case-mangled'),      'test@example.com', 'filter: good-case-mangled' );
+    is(  $results->valid('good-case-preserved'),    'TEST@eXaMpLe.CoM', 'filter: good-case-preserved' );
+    ok( !$results->valid('bad-no-domain'),          'filter: bad-no-domain' );
+    ok( !$results->valid('bad-no-user'),            'filter: bad-no-user' );
+    ok( !$results->valid('bad-not-email'),          'filter: bad-not-email' );
 }
 
 ###############################################################################
-test_constrain_email_valid: {
+test_constraint: {
     my $results = Data::FormValidator->check(
-        { 'good'    => 'cpan@howlingfrog.com',
-          'bad'     => 'invalid@host.doesnt.exist.howlingfrog.com',
+        { 'good'        => 'test@example.com',
+          'bad-no-mx'   => 'test@this-domain-doesnt-exist-anywhere.com',
         },
-        { 'required' => [qw( good bad )],
+        { 'required' => [qw(
+            good
+            bad-no-mx
+            )],
           'constraint_methods' => {
-              'good' => constrain_email_valid(),
-              'bad'  => constrain_email_valid(),
+              'good'        => FV_email(),
+              'bad-no-mx'   => FV_email(),
           },
         } );
-    ok(  $results->valid('good'), 'positive test for constrain_email_valid' );
-    ok( !$results->valid('bad'),  'negative test for constrain_email_valid' );
+    is(  $results->valid('good'),               'test@example.com', 'constraint: good' );
+    ok( !$results->valid('bad-no-mx'),          'constraint: bad-no-mx' );
 }
